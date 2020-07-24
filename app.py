ea29785ccf8cdfe5ed93c77af585ae8315875874
Matt Bayer <meb287@cornell.edu>
2020-07-23 21:18:29 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import json
+import datetime
+from flask import Flask, request, render_template, abort, flash, redirect, url_for
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+app.secret_key = b"fgp2938fsdf?/"
+app.config["DEBUG"] = True
+
+
+def is_last_week(date_str):
+    m, d, y = date_str.split("/")
+    event_date = datetime.date(int(y) + 2000, int(m), int(d))
+    time_since = datetime.date.today() - event_date
+    return time_since.days <= 7
+
+
+def get_data_index():
+    with open("data/data.json", "r") as f:
+        data = json.loads(f.read())
+
+    schools = []
+    for school_json in data:
+        school_data = {}
+        school_data["total_cases"] = 0
+        school_data["cases_past_week"] = 0
+        school_data["name"] = school_json["Name"]
+        for event in school_json["Events"]:
+            school_data["total_cases"] += dict(event).get("New Cases", 0)
+            if is_last_week(event["Date"]):
+                school_data["cases_past_week"] += dict(event).get("New Cases", 0)
+        school_data["cases_today"] = 0
+        schools.append(school_data)
+    schools = sorted(schools, key=lambda s: s["total_cases"])
+    schools.reverse()
+    for rank, school in enumerate(schools):
+        school["rank"] = rank + 1
+    total = sum([school["total_cases"] for school in schools])
+    return schools, total
+
+
+def get_data_graph(school_name):
+    with open("data/data.json", "r") as f:
+        data = json.loads(f.read())
+
+    schools = []
+    for school_json in data:
+        if (
+            school_json["Name"].lower().replace(" ", "-").replace(",", "")
+            == school_name
+        ):
+            return school_json
+    return None
+
+
+def get_names():
+    with open("data/data.json", "r") as f:
+        data = json.loads(f.read())
+    return [d["Name"] for d in data]
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        schools, total = get_data_index()
+        return render_template(
+            "index.html", schools=schools, total=total, names=get_names()
+        )
+    elif request.method == "POST":
+        college = request.form.to_dict(flat=True)["name"]
+        return redirect(f"colleges/{college}")
+
+
+@app.route("/contribute", methods=["GET", "POST"])
+def contribute():
+    if request.method == "GET":
+        return render_template("contribute.html")
+    elif request.method == "POST":
+        with open("data/contribute.json", "a") as f:
+            json.dump(request.form.to_dict(flat=True), f)
+            f.write("\n")
+            flash("Successfully submitted.")
+            return redirect(url_for("index"))
+        return render_template("contribute.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "GET":
+        return render_template("contact.html")
+    elif request.method == "POST":
+        with open("data/contact.json", "a") as f:
+            json.dump(request.form.to_dict(flat=True), f)
+            f.write("\n")
+            flash("Successfully submitted.")
+            return redirect(url_for("index"))
+        return render_template("contact.html")
+
+
+@app.route("/colleges/<name>")
+def college(name):
+    school = get_data_graph(name)
+    if school is None:  # school not found in data
+        abort(404)
+    else:
+        return render_template("college.html", school=school)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
---
+++ static/js/barchart.js
@@ -0,0 +1,128 @@
+var ctxL = document.getElementById("lineChart").getContext('2d');
+var schooldata = JSON.parse(document.querySelector('#schooldata').dataset.school);
+
+var july1 = new Date().setFullYear(2020, 6, 1);
+var days_since_july1 = Math.round((new Date() - july1) / 86400000);
+var data = new Array(days_since_july1).fill(0);
+var date_labels = [];
+var start = new Date()
+start.setFullYear(2020, 6, 1);
+for (i = 0; i < data.length; i++) {
+  date_str = new Date(start).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+  date_labels.push(date_str);
+  start.setDate(start.getDate() + 1);
+}
+
+console.log(data);
+console.log(date_labels);
+
+schooldata.Events.forEach(function (obj) {
+  var dates = obj.Date.split("/");
+  var month = dates[0];
+  var day = dates[1];
+  var year = dates[2];
+  var date = new Date().setFullYear(parseInt(year, 10) + 2000,
+    parseInt(month, 10) - 1, // months start at 0
+    parseInt(day, 10));
+  var days_since_july1 = Math.round((date - july1) / 86400000);
+  data[days_since_july1] = obj["New Cases"];
+});
+
+var cumsum = 0;
+var total_cases = new Array(days_since_july1).fill(0);
+for (i = 0; i < data.length; i++) {
+  cumsum += data[i];
+  total_cases[i] = cumsum;
+}
+
+console.log(date_labels);
+
+var myLineChart = new Chart(ctxL, {
+  type: 'line',
+  data: {
+    labels: date_labels,
+    datasets: [{
+      label: "New Daily Cases",
+      data: data,
+      backgroundColor: [
+        'rgba(105, 0, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(200, 99, 132, .7)',
+      ],
+      borderWidth: 2
+    },
+    {
+      label: "Total Cases",
+      data: total_cases,
+      backgroundColor: [
+        'rgba(0, 137, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(0, 10, 130, .7)',
+      ],
+      borderWidth: 2
+    }
+    ]
+  },
+  options: {
+    responsive: true,
+  }
+});
+/*var myLineChart = new Chart(ctxL, {
+  type: 'scatter',
+
+  data: {
+    labels: ["1", "2", "3"],
+    data: [4, 5, 6],
+
+  },
+  options: {
+    responsive: true,
+  }
+});*/
+/*  type: 'scatter',
+  data: {
+    datasets: [{
+      labels: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
+      label: "New Cases per day",
+      data: data,
+      backgroundColor: [
+        'rgba(105, 0, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(200, 99, 132, .7)',
+      ],
+      borderWidth: 2,
+      //showLine: true
+    }
+    ],
+    options: {
+      responsive: true,
+      scales: {
+        xAxes: [{
+          //type: 'time',
+          time: {
+            min: Date.parse('2020/07/01 00:00:00'),
+            max: Date.parse('2020/08/01 00:00:00'),
+            parser: 'YYYY-MM-DD HH:mm:ss'
+          },
+          //  unit: 'millisecond',
+          //  distribution: "linear",
+          //  minunit: "day",
+          //displayFormats: { month: "MMM YYYY" }
+          //},
+          ticks: {
+            source: "data",
+            userCallback: function (value, index, values) {
+              console.log(new Date(value).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' }));
+              return new Date(value).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+              //return new Date(value).toLocaleDateString("DD/MM/YY", { day: "long", month: 'short', year: 'numeric' });
+            }
+          }
+        }]
+      }
+    }
+  }
+}
+);*/
