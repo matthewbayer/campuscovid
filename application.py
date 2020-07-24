d042413e4291266f4bc344f9a381d82bceeacb28
Matt Bayer <meb287@cornell.edu>
2020-07-24 16:14:53 -0400

    ADD: eb support FIX: search bar bug

--- application.py
+++ application.py
@@ -2,8 +2,8 @@ import json
 import datetime
 from flask import Flask, request, render_template, abort, flash, redirect, url_for
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
-app.secret_key = b"fgp2938fsdf?/"
+application = Flask(__name__, static_url_path="/static")
+application.secret_key = b"fgp2938fsdf?/"
 
 
 def is_last_week(date_str):
@@ -57,7 +57,7 @@ def get_names():
     return [d["Name"] for d in data]
 
 
-@app.route("/", methods=["GET", "POST"])
+@application.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         schools, total = get_data_index()
@@ -69,45 +69,45 @@ def index():
         return redirect(f"colleges/{college}")
 
 
-@app.route("/contribute", methods=["GET", "POST"])
+@application.route("/contribute", methods=["GET", "POST"])
 def contribute():
     if request.method == "GET":
-        return render_template("contribute.html")
+        return render_template("contribute.html", names=get_names())
     elif request.method == "POST":
-        with open("data/contribute.json", "a") as f:
+        with open("/var/log/app-logs/contribute.json", "a") as f:
             json.dump(request.form.to_dict(flat=True), f)
             f.write("\n")
             flash("Successfully submitted.")
             return redirect(url_for("index"))
-        return render_template("contribute.html")
+        return render_template("contribute.html", names=get_names())
 
 
-@app.route("/contact", methods=["GET", "POST"])
+@application.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "GET":
-        return render_template("contact.html")
+        return render_template("contact.html", names=get_names())
     elif request.method == "POST":
-        with open("data/contact.json", "a") as f:
+        with open("/var/log/app-logs/contact.json", "a") as f:
             json.dump(request.form.to_dict(flat=True), f)
             f.write("\n")
             flash("Successfully submitted.")
             return redirect(url_for("index"))
-        return render_template("contact.html")
+        return render_template("contact.html", names=get_names())
 
 
-@app.route("/colleges/<name>")
+@application.route("/colleges/<name>")
 def college(name):
     school = get_data_graph(name)
     if school is None:  # school not found in data
         abort(404)
     else:
-        return render_template("college.html", school=school)
+        return render_template("college.html", school=school, names=get_names())
 
 
-@app.errorhandler(404)
+@application.errorhandler(404)
 def not_found(e):
-    return render_template("404.html")
+    return render_template("404.html", names=get_names())
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
